b5cfcddf7de598f0240a617349610654b14149a4
Joyce <joycalation@gmail.com>
2019-02-27 14:59:13 -0500

    Passed movie details to details view controller, set up tab bar, grid/collection view layout

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joyce Huang on 2/27/19.
+//  Copyright © 2019 Joyce Huang. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
